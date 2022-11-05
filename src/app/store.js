93c7845fb87f71a111e80cb6eb31794e57873d0a
Raynaldo Sutisna <raaynaldo@gmail.com>
2022-11-05 16:49:09 -0400

    feat: remove pokemon api

--- src/app/store.js
+++ src/app/store.js
@@ -2,20 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { jsonServerApi } from './services/jsonServerApi';
-import { pokemonApi } from './services/pokemon';
 
 export const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
-    [pokemonApi.reducerPath]: pokemonApi.reducer,
     [jsonServerApi.reducerPath]: jsonServerApi.reducer,
   },
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware()
-      .concat(pokemonApi.middleware)
-      .concat(jsonServerApi.middleware),
+    getDefaultMiddleware().concat(jsonServerApi.middleware),
 });
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
