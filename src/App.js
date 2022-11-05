a146c95a3d88b3b8e67bda25580ea7a11f9b2d9f
Raynaldo Sutisna <raaynaldo@gmail.com>
2022-11-05 15:43:52 -0400

    feat: update app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import styles from './App.module.css';
+import Albums from './features/Albums/Albums';
+import Photos from './features/photos/Photos';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.wrapper}>
+      <Photos />
+      <hr />
+      <Albums />
     </div>
   );
 }
-
-export default App;
